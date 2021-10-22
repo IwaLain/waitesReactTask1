4725b1f1137005610b50502cc5202adff0abaa8f
IwaLain <arthes.menetil@gmail.com>
2021-10-22 14:10:52 +0300

    added Todo Edit action

---
+++ src/components/EditModal.js
@@ -0,0 +1,32 @@
+import { useState, useRef } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap'
+
+const EditModal = ({ buttonTitle, editTodo, todoId }) => {
+    const [modal, setModal] = useState(false)
+    const [textToAdd, setTextToAdd] = useState('')
+
+    const toggle = () => setModal(!modal)
+
+    return (
+        <div>
+            <Button color="secondary" onClick={() => toggle()}>{buttonTitle}</Button>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Edit Todo</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="new-title">New Title</Label>
+                            <Input id="new-title" placeholder="Write a new title" onChange={(e) => setTextToAdd(e.target.value)}/>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={() => { editTodo(todoId, textToAdd); toggle() }}>Save</Button>
+                    <Button color="secondary" onClick={() => toggle()}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    )
+}
+
+export default EditModal
